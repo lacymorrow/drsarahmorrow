bf1231d6d81520009ad650e97cb240fd6bc6d9dc
Lacy Morrow <me@lacymorrow.com>
2024-04-27 15:01:16 -0400

    go!

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      // Basic redirect
+      {
+        source: '/',
+        destination: 'https://youtube.com/live/hOhg7-C0m6o?feature=share',
+        permanent: false,
+      }
+	]
+  },
+}
 
 export default nextConfig;
